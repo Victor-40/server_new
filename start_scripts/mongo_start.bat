eb981da812dafda54a7d2b23d2db9f76deabd24a
Victor Cherezov <victor_cherezov@mentor.com>
2020-09-22 10:17:58 +0300

    add start scripts

---
+++ start_scripts/mongo_start.bat
@@ -0,0 +1,1 @@
+mongod --dbpath="c:\production_svelte\mongodb"
---
+++ start_scripts/serve-sqlite.bat
@@ -0,0 +1,7 @@
+
+cd /d c:\production_svelte\server_new
+start c:\Python37-32\Scripts\flask.exe run -h 0.0.0.0 --port 5001
+
+timeout /t 2
+cd /d c:\production_svelte\vue_client
+npm run serve
